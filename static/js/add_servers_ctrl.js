dfb2194472b0d961105e8afc2a4ad7ff8af12d43
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-11-19 18:41:44 +0100

    fixed api proxy

--- static/js/add_servers_ctrl.js
+++ static/js/add_servers_ctrl.js
@@ -7,7 +7,7 @@ function (angular, module) {
   module.controller('AddServersCtrl', function($scope, backendSrv, $location) {
     $scope.server = { "hostnme": "", "ip": ""};
     $scope.save = function() {
-      backendSrv.post('external-example/api/servers', $scope.server).then(function(res) {
+      backendSrv.post('/api/plugin-proxy/external-example/api/servers', $scope.server).then(function(res) {
         $location.path("example/servers");
       });
     };
--- static/js/list_servers_ctrl.js
+++ static/js/list_servers_ctrl.js
@@ -5,7 +5,7 @@ define([
 function (angular, module) {
 
   module.controller('ListServersCtrl', function($scope, backendSrv) {
-    backendSrv.get('external-example/api/servers').then(function(res) {
+    backendSrv.get('api/plugin-proxy/external-example/api/servers').then(function(res) {
         $scope.servers = res.servers;
     });
   });
